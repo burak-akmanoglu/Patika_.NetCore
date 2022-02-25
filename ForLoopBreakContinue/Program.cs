579ecf399ee9e6d234123ecf5017da4205d3a622
burak akmanoglu <ba>
2022-02-25 22:36:39 +0300

    For Loop Break Continue

---
+++ ForLoopBreakContinue/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ForLoopBreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i; //tekToplam = tektoplam + i;
+                }
+                else
+                {
+                    ciftToplam += i; //ciftToplam =ciftToplam + i;
+                }
+            }
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+            //break, continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+
+                }
+                Console.WriteLine(i);
+
+            }
+
+
+        }
+    }
+}
