333261b7cb5bfa32874ac3719f339786c3e5eb6a
burak akmanoglu <ba>
2022-02-25 20:41:42 +0300

    ifelse

---
+++ IfElseIf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfElseIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+            sonuc = time >= 6 && time < 11 ? "İyi Günler" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
