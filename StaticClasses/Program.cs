269109673f5d4ce8e10da9f0e4e09dc95f4287a3
burak akmanoglu <ba>
2022-03-19 11:32:42 +0300

    StaticClasses

---
+++ StaticClasses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Burak", "Akmanoglu", "IT");
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+            Calisan calisan1 = new Calisan("Halil", "ibrahim", "IT");
+            Calisan calisan2 = new Calisan("Ali", "Veli", "IT");
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+
+
+            Console.WriteLine("Toplama işlemi Sonucu :{0}",Islemler.Topla(100,200));
+            Console.WriteLine("Çıkarma işleme sonucu:{0}",Islemler.Cikar(400,50));
+        }
+    }
+    class Calisan
+    {
+        private static int calisanSayisi;
+        public static int CalisanSayisi { get => calisanSayisi; set => calisanSayisi = value; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Departman = departman;
+            this.Soyisim = soyisim;
+            calisanSayisi++;
+        }
+
+    }
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
